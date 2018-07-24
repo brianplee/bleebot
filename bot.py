4819d51f1705726fcbc8ec0bbc06dfe5e60bacef
BPL <9127593+brianplee@users.noreply.github.com>
2018-07-23 22:16:50 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
  
-#to get updated discord.py, CMD: pip install -U https://github.com/Rapptz/discord.py/archive/rewrite.zip
+#Locally: CMD ->pip install -U https://github.com/Rapptz/discord.py/archive/rewrite.zip
+#Declare dependents on Heroku: 
 import datetime
 from datetime import time
 from datetime import datetime
@@ -10,12 +11,10 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import os 
 
-#Getting api key from heroku
+#Get token from heroku config'd var 
 bot_token = os.environ['BOT_TOKEN']
 
 bot = commands.Bot(command_prefix='-')
-                   
-#the command_prefix is what must be typed to invoke a command
 
 @bot.event
 async def on_ready():
@@ -24,15 +23,11 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-
 #on_ready() called when the client is done preparing data received from Discord
 #usually after bot's login is successful
-
 #===============================================================================
 #Bot functions to invoke commands
 #ctx = context
-#def function_name(ctx, arg1, arg2...)
-#enter "prefix+function argument" ('-hatchesin 120')
 
 @bot.command()
 async def info(ctx):
@@ -51,7 +46,7 @@ async def guide(ctx):
     embed.add_field(name="-info", value="Gives info about this bot")
     embed.add_field(name="-tip price percent", value="Calculates your tip.  Ex) -tip 20 15")
     embed.add_field(name="-hatchesin minutes HH:MMam/pm", value="Gives the hatch time and despawn time given minutes left until hatch.  Ex) -hatchesin 45 9:30am")
-    embed.add_field(name="-hatchTime HH:MMam/pm", value="Gives the despawn time given minutes left until hatch")
+    embed.add_field(name="-hatchTime HH:MMam/pm", value="Gives the despawn time given the hatch time.")
     embed.add_field(name="-timeleft minutes HH:MMam/pm", value="Gives the despawn time given minutes left until despawn.  Ex) -timeleft 45 9:30am")
     await ctx.send(embed=embed)
     
@@ -72,14 +67,6 @@ async def tip(ctx, a: float, b: float):
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hi.  I am Bleebot.")
 
-#TO DO: MemberExporter ========================================================================
-    #export list of members with team affliation to csv
-    
-#TO DO: GymDataBase ===========================================================================
-    #store gym name, googlemaps link, and description in free database (google sheets)
-    #call gym info from google sheets
-
-
 #Despawn from minutes until hatch ======================================================
 @bot.command()
 async def hatchesin(ctx, a: int, b):
@@ -99,21 +86,26 @@ async def hatchesin(ctx, a: int, b):
 async def hatchtime(ctx, a):
     hatchesAt = datetime.strptime(a, "%H:%M%p")
     raidDuration = timedelta(minutes=45)
-    await ctx.send("Despawns at:", hatchesAt + raidDuration)
+    despawnTime = hatchesAt + raidDuration
+    await ctx.send("Hatches at: {}".format(a))
+    await ctx.send("Despawns at: {}".format(despawnTime))
 
 #Despawn from time remaining on boss ====================================================
 @bot.command()
 async def timeleft(ctx, a: int, b):
     timeRemaining = timedelta(minutes=a)
     timeReported = datetime.strptime(b, "%H:%M%p")
-    await ctx.send("Minutes before despawn:", a, "minutes.")
-    await ctx.send("Despawn time:", timeReported + timeRemaining)
-
-#if __name__ == '__main__':
-    #import config
-    #client.run(config.token)
-    #bot.run(Token)
+    despawnTime = timeReported + timeRemaining
+    await ctx.send("Despawns in {} minutes".format(a))
+    await ctx.send("Despawns at {}".format(despawnTime))
+ 
+#TO DO: MemberExporter ========================================================================
+    #export list of members with team affliation to csv
     
+#TO DO: GymDataBase ===========================================================================
+    #store gym name, googlemaps link, and description in free database (google sheets)
+    #call gym info from google sheets
+  
 #bot.run(str(os.environ.get('BOT_TOKEN')))
 bot.run(bot_token)
 
