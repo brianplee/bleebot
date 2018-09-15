69f5019b6087040bbd1396ee0aff3d81e6a57a6e
BPL <9127593+brianplee@users.noreply.github.com>
2018-09-14 23:52:52 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ CLIENT_SECRET = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bleebot%40gymdatabase-215200.iam.gserviceaccount.com"
+  "client_x509_cert_url": os.environ['CLIENT_X509']
 }
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
